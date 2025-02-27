f25caad106e77d4749f9d9a28014b33407b1949d
kaiserfromwork <kaiserfromwork@gmail.com>
2025-02-27 11:48:32 -0300

    UserDatabase class created

---
+++ lesson_2/UserDatabase.py
@@ -0,0 +1,13 @@
+class UserDatabase:
+
+    def insert(self, user):
+        pass
+
+    def find(self, username):
+        pass
+
+    def update(self, user):
+        pass
+
+    def list_all(self):
+        pass
