b41e8072f0e0b50f02a8a2476c2002992fe19f69
kaiserfromwork <kaiserfromwork@gmail.com>
2025-02-27 15:05:11 -0300

    userdatabase class working

--- lesson_2/UserDatabase.py
+++ lesson_2/UserDatabase.py
@@ -1,13 +1,35 @@
 class UserDatabase:
 
+    def __init__(self):
+        self.user_database = []
+
     def insert(self, user):
-        pass
+        if not self.user_database:
+            self.user_database.append(user)
+            return f'User: {user} added!'
+
+        if self.find(user.username) is None:
+            for index in range(len(self.user_database)):
+                if user.username < self.user_database[index].username:
+                    self.user_database.insert(index, user)
+                    return f'User: {user} added!'
+            self.user_database.append(user)
+            return f'User: {user} added!'
+        else:
+            return f'User: {user} already exists'
 
     def find(self, username):
-        pass
+        for x in range(len(self.user_database)):
+            if username == self.user_database[x].username:
+                return self.user_database[x]
 
     def update(self, user):
-        pass
+        user_found = self.find(user.username)
+        if user_found:
+            user_found.name, user_found.email = user.name, user.email
+            print("User Updated!")
+        else:
+            print("User not Found!")
 
     def list_all(self):
-        pass
+        return self.user_database  # Returns a list of the database
