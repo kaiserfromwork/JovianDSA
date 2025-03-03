c49522144e1d9a14e92ed915ea0a791666faef1d
kaisefromwork <kaiserfromwork@gmail.com>
2025-03-03 11:22:48 -0300

    added comments to height_of_tree function

--- lesson_2/bst.py
+++ lesson_2/bst.py
@@ -72,6 +72,6 @@ class BST:
 
     def height_of_tree(self, node):
         if node is None:
-            return -1
+            return -1  # Set to -1 so leaf node can have height of zero
 
         return 1 + max(self.height_of_tree(node.left), self.height_of_tree(node.right))
